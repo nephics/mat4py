97ca201f31c618cfeea7049ea84f8df2f40f6ec1
Jacob Sondergaard <jacob@nephics.com>
2014-10-22 21:04:27 +0200

    fix install README issue

--- mat4py/mat4py.py
+++ mat4py/mat4py.py
@@ -28,7 +28,7 @@ The following Matlab data structures/types are not supported:
 * Anonymous function classes
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ['loadmat', 'savemat']
 __license__ = """The MIT License (MIT), Copyright (c) 2011-2014 Nephics AB"""
 
--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from distutils.core import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name='mat4py',
-    version='0.1.0',
+    version='0.1.1',
     author='Jacob Sondergaard',
     author_email='jacob@nephics.com',
     packages=['mat4py'],
     url='http://pypi.python.org/pypi/mat4py/',
     license='MIT License',
     description='Load and save data in the Matlab (TM) MAT-file format.',
-    long_description=open('README.rst').read(),
+    long_description=readme(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
