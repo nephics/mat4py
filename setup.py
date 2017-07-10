523e3b062fd97de55038de70bc1d687f828aecb7
Jacob Svensson <jacob@nephics.com>
2017-07-10 13:36:03 +0200

    Move to github

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Jacob Svensson',
     author_email='jacob@nephics.com',
     packages=['mat4py'],
-    url='https://bitbucket.org/nephics/mat4py/',
+    url='https://github.com/nephics/mat4py/',
     license='MIT License',
     description='Load and save data in the Matlab (TM) MAT-file format.',
     long_description=readme(),
